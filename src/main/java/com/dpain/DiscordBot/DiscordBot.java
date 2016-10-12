5ea1ee99fd5f61229cf73f6ecbc108dd13728de9
DPain Hahn <coreymu@naver.com>
2016-10-12 19:38:03 -0400

    Added an easy way to change profile picture before running the bot.

--- src/main/java/com/dpain/DiscordBot/DiscordBot.java
+++ src/main/java/com/dpain/DiscordBot/DiscordBot.java
@@ -1,5 +1,8 @@
 package com.dpain.DiscordBot;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+
 import javax.security.auth.login.LoginException;
 
 import com.dpain.DiscordBot.enums.Property;
@@ -13,6 +16,8 @@ import com.dpain.DiscordBot.system.UserManager;
 
 import net.dv8tion.jda.JDA;
 import net.dv8tion.jda.JDABuilder;
+import net.dv8tion.jda.utils.AvatarUtil;
+import net.dv8tion.jda.utils.AvatarUtil.Avatar;
 
 public class DiscordBot {
 	private JDA jda;
@@ -33,6 +38,8 @@ public class DiscordBot {
 			UserEventListener.setDefaultGuild(jda.getGuildById(PropertiesManager.load().getValue(Property.GUILD_ID)));
 			
 			System.out.println(ConsolePrefixGenerator.getFormattedPrintln("DiscordBot", "Running!"));
+			
+			//changeAvatar();
 		} catch (LoginException e) {
 			System.out.println("The provided Login information is incorrect. Please provide valid details.");
 		} catch (IllegalArgumentException e) {
@@ -44,5 +51,17 @@ public class DiscordBot {
 	
 	public void readConsole() {
 		(new Thread(new ConsoleInputReader(jda, pluginListener, jda.getGuildById(PropertiesManager.load().getValue(Property.GUILD_ID))))).start();
+	}
+	
+	private void changeAvatar() {
+		Avatar avatar;
+		try {
+			avatar = AvatarUtil.getAvatar(new File("File Path"));
+			jda.getAccountManager().setAvatar(avatar);
+			jda.getAccountManager().update();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
