db85b14abd5ff1ab04844f218456c8021bc51853
DPain <d.pain.coreymu@gmail.com>
2019-02-15 16:58:17 -0500

    Fixed Message deleter, "Cleaner". Probably broke during the REST API revamp.

--- src/main/java/com/dpain/DiscordBot/plugin/ModeratorPlugin.java
+++ src/main/java/com/dpain/DiscordBot/plugin/ModeratorPlugin.java
@@ -6,121 +6,124 @@ import java.util.Map;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import com.dpain.DiscordBot.enums.Group;
 import com.dpain.DiscordBot.enums.Timezone;
 import com.dpain.DiscordBot.helper.LogHelper;
 import com.dpain.DiscordBot.plugin.moderator.Cleaner;
-
 import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
 public class ModeratorPlugin extends Plugin {
-	private final static Logger logger = Logger.getLogger(ModeratorPlugin.class.getName());
+  private final static Logger logger = Logger.getLogger(ModeratorPlugin.class.getName());
 
-	public ModeratorPlugin() {
-		super("ModeratorPlugin", Group.MODERATOR);
-		
-		super.helpString = "**Moderator Plugin Usage:**\n"
-				+ "-nick *\"name\"* : Changes the nickname of the bot.\n"
-				+ "-channel : Returns some info of the current Channel.\n"
-				+ "-randomnick : Randomly changes the nickname of the bot.\n"
-				+ "-clear *\"channelName\"* : Clears all the messages in the text channel\n";
-		EssentialsPlugin.appendHelpString(super.helpString);
-	}
+  public ModeratorPlugin() {
+    super("ModeratorPlugin", Group.MODERATOR);
 
-	@Override
-	public void handleEvent(Event event) {
-		if(event instanceof GuildMessageReceivedEvent) {
-			try {
-				GuildMessageReceivedEvent castedEvent = (GuildMessageReceivedEvent) event;
-				String message = castedEvent.getMessage().getContentRaw();
-		        
-				if((castedEvent.getAuthor().getId().equals(event.getJDA().getSelfUser().getId())) || canAccessPlugin(castedEvent.getMember())) {
-					
-					if(message.startsWith("-")) {
-		                if (message.startsWith("-nick ")) {
-		            		String param = message.substring(6);
-		            		
-		            		castedEvent.getGuild().getController().setNickname(castedEvent.getGuild().getSelfMember(), param).queue();
-		            		castedEvent.getChannel().sendMessage("**Nickname changed to:** " + param).queue();
-		            		
-		            		logger.log(Level.INFO, LogHelper.elog(castedEvent, String.format("Command: %s", message)));
-		            	} else if (message.equals("-channel")) {
-		            		Map<String, String> channelInfo = new LinkedHashMap<String, String>();
-		            		channelInfo.put("ID", castedEvent.getChannel().getId());
-		            		channelInfo.put("Name", castedEvent.getChannel().getName());
-		            		channelInfo.put("Topic", castedEvent.getChannel().getTopic());
-		            		channelInfo.put("Channel Type", castedEvent.getChannel().getType().toString());
-		            		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
-		            		String formattedDateTime = castedEvent.getChannel().getCreationTime().format(formatter);
-		            		channelInfo.put("Created Date", formattedDateTime + " " + Timezone.EST.getZoneId());
-		            		channelInfo.put("NSFW", Boolean.toString(castedEvent.getChannel().isNSFW()));
-		            		if(castedEvent.getChannel().getParent() != null) {
-		            			channelInfo.put("Category", castedEvent.getChannel().getParent().getName());
-			            		channelInfo.put("Category ID", castedEvent.getChannel().getParent().getId());
-		            		}
-		            		channelInfo.put("# of Pinned Messages", Integer.toString(castedEvent.getChannel().getPinnedMessages().complete().size()));
-		            		
-		            		String formattedString = "";
-		            		for(String category : channelInfo.keySet()) {
-		            			formattedString += String.format("%s: %s\n", category, channelInfo.get(category));
-		            		}
-		            		formattedString = formattedString.trim();
-		            		
-		            		castedEvent.getChannel().sendMessage("**Channel Info:**\n" + formattedString).queue();
-		            	} else if (message.equals("-randomnick")) {
-		            		String[] names = {"Malfurion",
-		            				"OpenAI",
-		            				"Jaina",
-		            				"Uther",
-		            				"Anduin",
-		            				"Valeera",
-		            				"Thrall",
-		            				"Gul'dan",
-		            				"Garrosh",
-		            				"Medivh",
-		            				"Dildo",
-		            				"2B",
-		            				"Toba",
-		            				"Kizuna Ai"};
-		            		
-		            		Random ran = new Random();
-		            		String tempName;
-		            		while(true) {
-		            			tempName = names[ran.nextInt(names.length)] + " Bot";
-		            			if(castedEvent.getGuild().getSelfMember().getNickname() == null || !castedEvent.getGuild().getSelfMember().getNickname().equals(tempName)) {
-		            				break;
-		            			}
-		            		}
-		            		
-		            		castedEvent.getGuild().getController().setNickname(castedEvent.getGuild().getSelfMember(), tempName).queue();
-		            		castedEvent.getChannel().sendMessage("**Nickname changed to:** " + tempName).queue();
-		            		
-		            		logger.log(Level.INFO, LogHelper.elog(castedEvent, String.format("Command: %s", message)));
-		            	} else if (message.startsWith("-clear ")) {
-		            		String param = message.substring(7);
-		            		try {
-		            			int i = Integer.parseInt(param);
-		            			if(!Cleaner.isRunning()) {
-		            				Thread clearProcess = new Thread(new Cleaner(castedEvent.getChannel(), i));
-				            		clearProcess.start();
-				            		
-				            		logger.log(Level.INFO, LogHelper.elog(castedEvent, String.format("Command: %s", message)));
-		            			}
-		            			// No point on sending a message that the cleaner is already running since it will get instantly deleted.
-		            		} catch(NumberFormatException e) {
-		            			castedEvent.getChannel().sendMessage("**Please enter a correct number!**").queue();
-		            			
-		            			logger.log(Level.WARNING, LogHelper.elog(castedEvent, String.format("Incorrect command: %s", message)));
-		            		}
-		            	}
-					}
-				}
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    super.helpString =
+        "**Moderator Plugin Usage:**\n" + "-nick *\"name\"* : Changes the nickname of the bot.\n"
+            + "-channel : Returns some info of the current Channel.\n"
+            + "-randomnick : Randomly changes the nickname of the bot.\n"
+            + "-clear *\"x\"* : Clears x amount of messages in the current text channel\n";
+    EssentialsPlugin.appendHelpString(super.helpString);
+  }
+
+  @Override
+  public void handleEvent(Event event) {
+    if (event instanceof GuildMessageReceivedEvent) {
+      try {
+        GuildMessageReceivedEvent castedEvent = (GuildMessageReceivedEvent) event;
+        String message = castedEvent.getMessage().getContentRaw();
+
+        if ((castedEvent.getAuthor().getId().equals(event.getJDA().getSelfUser().getId()))
+            || canAccessPlugin(castedEvent.getMember())) {
+
+          if (message.startsWith("-")) {
+            if (message.startsWith("-nick ")) {
+              String param = message.substring(6);
+
+              castedEvent.getGuild().getController()
+                  .setNickname(castedEvent.getGuild().getSelfMember(), param).queue();
+              castedEvent.getChannel().sendMessage("**Nickname changed to:** " + param).queue();
+
+              logger.log(Level.INFO,
+                  LogHelper.elog(castedEvent, String.format("Command: %s", message)));
+            } else if (message.equals("-channel")) {
+              Map<String, String> channelInfo = new LinkedHashMap<String, String>();
+              channelInfo.put("ID", castedEvent.getChannel().getId());
+              channelInfo.put("Name", castedEvent.getChannel().getName());
+              channelInfo.put("Topic", castedEvent.getChannel().getTopic());
+              channelInfo.put("Channel Type", castedEvent.getChannel().getType().toString());
+              DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
+              String formattedDateTime =
+                  castedEvent.getChannel().getCreationTime().format(formatter);
+              channelInfo.put("Created Date", formattedDateTime + " " + Timezone.EST.getZoneId());
+              channelInfo.put("NSFW", Boolean.toString(castedEvent.getChannel().isNSFW()));
+              if (castedEvent.getChannel().getParent() != null) {
+                channelInfo.put("Category", castedEvent.getChannel().getParent().getName());
+                channelInfo.put("Category ID", castedEvent.getChannel().getParent().getId());
+              }
+              channelInfo.put("# of Pinned Messages",
+                  Integer.toString(castedEvent.getChannel().getPinnedMessages().complete().size()));
+
+              String formattedString = "";
+              for (String category : channelInfo.keySet()) {
+                formattedString += String.format("%s: %s\n", category, channelInfo.get(category));
+              }
+              formattedString = formattedString.trim();
+
+              castedEvent.getChannel().sendMessage("**Channel Info:**\n" + formattedString).queue();
+            } else if (message.equals("-randomnick")) {
+              String[] names = {"Malfurion", "OpenAI", "Jaina", "Uther", "Anduin", "Valeera",
+                  "Thrall", "Gul'dan", "Garrosh", "Medivh", "Dildo", "2B", "Toba", "Kizuna Ai"};
+
+              Random ran = new Random();
+              String tempName;
+              while (true) {
+                tempName = names[ran.nextInt(names.length)] + " Bot";
+                if (castedEvent.getGuild().getSelfMember().getNickname() == null
+                    || !castedEvent.getGuild().getSelfMember().getNickname().equals(tempName)) {
+                  break;
+                }
+              }
+
+              castedEvent.getGuild().getController()
+                  .setNickname(castedEvent.getGuild().getSelfMember(), tempName).queue();
+              castedEvent.getChannel().sendMessage("**Nickname changed to:** " + tempName).queue();
+
+              logger.log(Level.INFO,
+                  LogHelper.elog(castedEvent, String.format("Command: %s", message)));
+            } else if (message.startsWith("-clear ")) {
+              String param = message.substring("-clear ".length());
+              try {
+                int i = Integer.parseInt(param);
+
+                try {
+                  Thread clearProcess = new Thread(new Cleaner(castedEvent.getChannel(), i));
+                  clearProcess.start();
+                } catch (RateLimitedException e) {
+                  String temp = "The bot is being rate limited!";
+                  castedEvent.getChannel().sendMessage(temp).queue();
+                  logger.log(Level.WARNING, LogHelper.elog(castedEvent, temp));
+                }
+
+                logger.log(Level.INFO,
+                    LogHelper.elog(castedEvent, String.format("Command: %s", message)));
+                // No point on sending a message that the cleaner is already running since it will
+                // get instantly deleted.
+              } catch (NumberFormatException e) {
+                castedEvent.getChannel().sendMessage("**Please enter a correct number!**").queue();
+
+                logger.log(Level.WARNING,
+                    LogHelper.elog(castedEvent, String.format("Incorrect command: %s", message)));
+              }
+            }
+          }
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
 }
--- src/main/java/com/dpain/DiscordBot/plugin/moderator/Cleaner.java
+++ src/main/java/com/dpain/DiscordBot/plugin/moderator/Cleaner.java
@@ -3,53 +3,47 @@ package com.dpain.DiscordBot.plugin.moderator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import com.dpain.DiscordBot.helper.LogHelper;
 import net.dv8tion.jda.core.entities.MessageHistory;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
 public class Cleaner implements Runnable {
-	private final static Logger logger = Logger.getLogger(Cleaner.class.getName());
-	
-	private static boolean isRunning = false;
-	
-	private MessageHistory messageHistory;
-	private int count;
-	
-	public Cleaner(TextChannel targetChannel, int i) {
-		messageHistory = new MessageHistory(targetChannel);
-		count = i + 1;
-	}
+  private final static Logger logger = Logger.getLogger(Cleaner.class.getName());
 
-	@Override
-	public void run() {
-		isRunning = true;
-		
-		List<Message> messages = messageHistory.getRetrievedHistory();
-		int i = 0;
-		while(messages != null && i < count) {
-			for(Message item: messages)	 {
+  private MessageHistory messageHistory;
+  private List<Message> messages;
+  private int count;
 
-				if(i >= count) {
-					logger.log(Level.INFO, String.format("Deleted %d messages.", count - 1));
-					isRunning = false;
-					return;
-				}
-				
-				item.delete();
-				i++;
-	        }
-	        if(messages.isEmpty())
-	            messages = messageHistory.getRetrievedHistory();
-	        if(messages == null) {
-	        	logger.log(Level.INFO, "No more messages left.");
-	        	isRunning = false;
-	        	return;
-	        }
-		}
-	}
-	
-	public static boolean isRunning() {
-		return isRunning;
-	}
+  public Cleaner(TextChannel targetChannel, int i) throws RateLimitedException {
+    logger.log(Level.INFO, String.format("Cleaner Initialized!"));
+
+    messageHistory = targetChannel.getHistory();
+    messages = messageHistory.retrievePast(i).complete(true);
+    
+    count = i + 1;
+  }
+
+  @Override
+  public void run() {
+    
+    int i = 0;
+    while (messages != null && i < count) {
+      for (Message item : messages) {
+
+        if (i >= count) {
+          logger.log(Level.INFO, String.format("Deleted %d messages.", count - 1));
+          return;
+        }
+
+        item.delete().queue();
+        i++;
+      }
+      if (messages == null) {
+        logger.log(Level.INFO, "No more messages left.");
+        return;
+      }
+    }
+  }
 }
